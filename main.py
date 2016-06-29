ec00b20fe29146f17f9ff374d568f54ba87080f8
cullo7 <culloaiden3@gmail.com>
2016-06-29 15:16:09 -0400

    more comments, added main

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import pygame
 
+######################################################################                          FUNCTIONS                                 # 
+#                             SETUP                                  #
+######################################################################
+
 (width,height) = (500,400)
 background_color = (0,0,0) #black
 block_color = (255,255,255) #white
+
+#giving a 10 pixel buffer on sides
 b_width = b_height = 10
-max_height = min_width = 10 #giving a 10 pixel buffer on sides
+max_height = min_width = 10
 min_height = 380
 max_width = 480
+
+#amount of height and width for 10x10 pixel boxe grid
 y_count = (height/b_height)-2
 x_count = (width/b_width)-2
+
+"""
+creating a x_count by y_count grid to keep track of squares already visited
+makeshift 2D array will be a list of lists
+"""
 grid = []
 for y in range(y_count):
 	grid.append([])
 	for x in range(x_count):
 		grid[y].append(x)
-	
+
+######################################################################
+#                          FUNCTIONS                                 # 
+######################################################################
+
+#prints block to screen -- called when mazerunner visits a square	
 def make_block(x, y): #print block to screen
 	pygame.draw.rect(screen, block_color, (x, y, b_width, b_height))
 
+#creates maze by stacking all options at intersections
 def create_maze():
 	
+
 
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Mazerunner')
@@ -34,4 +54,10 @@ while running:
 			running = False
 
 
+######################################################################
+#                          MAIN                                      # 
+######################################################################
+
+if __name__ == '__main__':
+	print("mazerunner beginning...")
 
