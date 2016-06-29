a00977995d892b8869df90edc45ed84285ead0b3
cullo7 <culloaiden3@gmail.com>
2016-06-29 11:31:46 -0400

    added block

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 
 (width,height) = (300,200)
+background_color = (0,0,0)
+block_color = (255,255,255)
+b_width = b_height = 10
+
+def make_block(x, y):
+	pygame.draw.rect(screen, block_color, (x, y, b_width, b_height))
+
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Mazerunner')
-background_color = (0,0,0)
 screen.fill(background_color)
+make_block(0, 190)
+
 pygame.display.flip()
 running = True
 while running:
