6d67796a68d4807b25af9d27ea11123400cadc7f
cullo7 <culloaiden3@gmail.com>
2016-06-29 12:51:32 -0400

    making boolean grid

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import pygame
 
-(width,height) = (300,200)
-background_color = (0,0,0)
-block_color = (255,255,255)
+(width,height) = (500,400)
+background_color = (0,0,0) #black
+block_color = (255,255,255) #white
 b_width = b_height = 10
-
-def make_block(x, y):
+max_height = min_width = 10 #giving a 10 pixel buffer on sides
+min_height = 380
+max_width = 480
+y_count = (height/b_height)-2
+x_count = (width/b_width)-2
+grid = []
+for y in range(y_count):
+	grid.append([])
+	for x in range(x_count):
+		grid[y].append(x)
+	
+def make_block(x, y): #print block to screen
 	pygame.draw.rect(screen, block_color, (x, y, b_width, b_height))
+
+def create_maze():
+	
 
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Mazerunner')
 screen.fill(background_color)
-make_block(0, 190)
+make_block(10, 380)
 
-pygame.display.flip()
+pygame.display.flip() #update screen
 running = True
 while running:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.QUIT: #quit if red X button pressed
 			running = False
 
 
