edc1038ed7b6bd1389d915356ca0cc8cacc56fc2
cullo7 <culloaiden3@gmail.com>
2016-07-02 14:33:13 -0400

    algorithm works, still needs to be cleaned up

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ import pygame, sys, time, random
 #                             SETUP                                  #
 ######################################################################
 
-(width,height) = (500,400)
+(width,height) = (510,410)
 background_color = (0,0,0) #black
 block_color = (255,255,255) #white
 
 #giving a 10 pixel buffer on sides
 b_width = b_height = 10
 min_height = min_width = 10
-max_height = 380
-max_width = 480
+max_height = 390
+max_width = 490
 
 #amount of height and width for 10x10 pixel boxe grid
 y_count = (max_height/b_height)
@@ -47,8 +47,8 @@ def make_block(x, y): #print block to screen
 	pygame.draw.rect(screen, block_color, (x, y, b_width, b_height))
 	print "in make block"
 
-def push(x,y):
-	positions.append([x,y])
+def push(x,y,DIR):
+	positions.append([x,y, DIR])
 
 def pop():
 	if (positions):
@@ -56,20 +56,61 @@ def pop():
 	else:
 		return -1
 
-def move_to(x,y):
+def check_point(x, y):
+	return not (x > max_width or x < min_width or y > max_height or y < min_height)
+
+def move_to(x, y, direction):
+	print '\n' +'\n'
+	print("Direction: ", direction)
 	print("move_to")
 	print("x ", x)
 	print("y ", y)
 	print("height ", len(grid))
 	print("width ", len(grid[0]))
-	if(x > max_width or x < min_width or y > max_height or y < min_height):
+	index_x = to_index_x(x)
+	index_y = to_index_y(y)
+
+	
+	if not check_point(x,y):
 		print("FAIL - OOB")
 		return False
-	x = (x/10)-1
-	y = 38 - (y/10)
-	if(grid[y][x] == True):
+	
+	if(grid[index_y][index_x]):
 		print("FAIL - EXISTS")
 		return False
+	
+	if(check_point(x-10,y) and  direction != "RIGHT" and grid[index_y][index_x-1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x-10,y-10) and  direction != "RIGHT" and direction != "DOWN" and grid[index_y+1][index_x-1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x,y-10) and  direction != "DOWN" and grid[index_y+1][index_x] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x+10,y-10) and  direction != "LEFT" and direction != "DOWN" and grid[index_y+1][index_x+1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x+10,y) and  direction != "LEFT" and grid[index_y][index_x+1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x+10,y+10) and  direction != "LEFT"  and direction != "UP" and grid[index_y-1][index_x+1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x,y+10) and  direction != "UP"  and grid[index_y-1][index_x] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
+	if(check_point(x-10,y+10) and  direction != "RIGHT" and direction != "UP" and grid[index_y-1][index_x-1] == True):
+		print("FAIL - BOUNDARY_EXISTS")
+		return False
+	
 	else:
 		print("SUCCESS")
 		return True
@@ -81,9 +122,15 @@ def discover(x,y):
 	print("disc")
 	make_block(x, y)
 	#adjusting to index the array
-	x = (x/10)-1
-	y = 38 - (y/10)
+	x = to_index_x(x)
+	y = to_index_y(y)
 	grid[y][x] = True
+
+def to_index_x(x):
+	return (x/10)-1
+
+def to_index_y(y):
+	return 39 - (y/10)
 
 ######################################################################
 #                          MAIN                                      # 
@@ -103,8 +150,7 @@ if __name__ == '__main__':
 	current_y = max_height
 	#possible coordinate system
 	cur_path = [] 
-	push(current_x, current_y)
-
+	push(current_x, current_y, "UP")
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: #quit if red X button pressed
@@ -112,35 +158,37 @@ if __name__ == '__main__':
 		
 		while not empty():
 				cur_path = pop()
-				print(cur_path[0],"<current>", cur_path[1])
-				if move_to(cur_path[0], cur_path[1]):
+				print(cur_path[0],"<current>", cur_path[1], "dir ", cur_path[2] )	
+				if(move_to(cur_path[0], cur_path[1], cur_path[2])):
+					
 					discover(cur_path[0], cur_path[1])	
 
-				temp_list = []
-				count = 0
-				
-				if(move_to(cur_path[0]+10,cur_path[1])):#to the right one
-					count +=1
-					temp_list.append((cur_path[0]+10,cur_path[1]))
-				
-				if(move_to(cur_path[0],cur_path[1]-10)):#up one
-					count +=1
-					temp_list.append((cur_path[0],cur_path[1]-10))
-				
-				if(move_to(cur_path[0]-10,cur_path[1])):#to the left one
-					count +=1
-					temp_list.append((cur_path[0]-10,cur_path[1]))
+					temp_list = []
+					count = 0
+					
+					if(move_to(cur_path[0]+10,cur_path[1], "RIGHT")):#to the right one
+						count +=1
+						temp_list.append((cur_path[0]+10,cur_path[1], "RIGHT"))
+					
+					if(move_to(cur_path[0],cur_path[1]-10, "UP")):#up one
+						count +=1
+						temp_list.append((cur_path[0],cur_path[1]-10, "UP"))
+					
+					if(move_to(cur_path[0]-10,cur_path[1], "LEFT")):#to the left one
+						count +=1
+						temp_list.append((cur_path[0]-10,cur_path[1], "LEFT"))
 
-				if(move_to(cur_path[0],cur_path[1]+10)):#down one
-					count +=1
-					temp_list.append((cur_path[0],cur_path[1]+10))
-				
-				while len(temp_list) != 0:
-					rand_num = random.randint(0, len(temp_list)-1)
-					print("***************Random integer: ", rand_num)
-					push(temp_list[rand_num][0], temp_list[rand_num][1])
-					temp_list.pop(rand_num)
+					if(move_to(cur_path[0],cur_path[1]+10, "DOWN")):#down one
+						count +=1
+						temp_list.append((cur_path[0],cur_path[1]+10, "DOWN"))
+					
+					while len(temp_list) != 0:
+						rand_num = random.randint(0, len(temp_list)-1)
+						push(temp_list[rand_num][0], temp_list[rand_num][1], temp_list[rand_num][2])
+						temp_list.pop(rand_num)
 
-				pygame.display.flip()
-		pygame.display.flip()
+					print("#################################################################################")
 
+					pygame.display.flip()
+	pygame.display.flip()
+	time.sleep(10)
